d462c0892ea3a7b72baa7a96e955cfd37bd39ee4
devvieira <kauavieira900@gmail.com>
2024-03-27 17:28:43 -0300

    UD-5  [WIP] add create voter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-'use client'
+"use client";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter()
-  return (
-    <main id="main">
-      <button onClick={() => {
-        router.push('/admin/cadastrarEleitor')
-      }}>Clique aqui
-      </button>
-    </main>
-  );
+	const router = useRouter();
+	return (
+		<main id="main">
+			<button
+				onClick={() => {
+					router.push("/admin/cadastrarEleitor");
+				}}
+			>
+				Clique aqui
+			</button>
+		</main>
+	);
 }
