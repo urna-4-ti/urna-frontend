e68ebc7fa8d9fb505402c1240d1b94e6dded5223
devvieira <kauavieira900@gmail.com>
2024-03-28 00:03:14 -0300

    UD-5  [WIP] add input-image

--- src/app/admin/cadastrarEleitor/page.tsx
+++ src/app/admin/cadastrarEleitor/page.tsx
@@ -4,10 +4,13 @@ import topCloud from "@/img/top-cloud.svg";
 import bottomCloud from "@/img/bottom-cloud.svg";
 import bottomCircle from "@/img/bottom-circle.svg";
 import logoUrna from "@/img/logo.svg";
+import inputImage from "@/img/image-input.svg";
 import { useState } from "react";
 
 export default function Home() {
 	const [selectValue, setSelectValue] = useState<string>("");
+
+	const [image, setImage] = useState<string | null>(null);
 
 	const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
 		setSelectValue(e.target.value);
@@ -45,10 +48,36 @@ export default function Home() {
 							<option value="1"></option>
 						</select>
 					</label>
+					<label htmlFor="inputImg" id="labelImg" tabIndex={0}>
+						<input
+							type="file"
+							name=""
+							id="inputImg"
+							accept="image/*"
+							onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+								const file = event.target.files?.[0]; // Optional chaining for potential absence of files
+								if (file) {
+									setImage(URL.createObjectURL(file));
+								}
+							}}
+						/>
+						{!image ? <Image src={inputImage} alt="Imagem Input"></Image> : ""}
+						{image && (
+							<Image
+								src={image}
+								alt="teste"
+								id="newImage"
+								width={1}
+								height={1}
+							></Image>
+						)}
+					</label>
 
-					<button>Cadastrar</button>
-					<a href=""></a>
-					<button id="cancelButton">Cancelar</button>
+					<div id="divButton">
+						<button>Cadastrar</button>
+
+						<button id="cancelButton">Cancelar</button>
+					</div>
 				</form>
 			</div>
 		</main>
