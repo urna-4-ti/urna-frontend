98a313137e9fdb00f6ebe2878c0704f5d57c149f
devvieira <kauavieira900@gmail.com>
2024-04-03 20:58:30 -0300

    UD-5  [WIP] test

--- src/app/admin/listarPartido/page.tsx
+++ src/app/admin/listarPartido/page.tsx
@@ -7,56 +7,54 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  return (
-    <main id="mainList">
-      <div id="leftDiv">
-        <Image src={logoUrna} alt="" id="logo"></Image>
-      </div>
-      <div id="rightDiv">
-        <div id="divTop">
-          <div id="left">
-            <h1>Listagem de Partidos</h1>
-          </div>
-          <div id="right">
-            <label htmlFor="">
-              <Image
-                src={iconUser}
-                alt="Ícone Usúario"
-                width={40}
-                height={40}
-              />
-              <div>
-                <p>Júlio Cesar Alencar</p>
-                <a href="">
-                  <span>Sair</span>
-                </a>
-              </div>
-            </label>
-          </div>
-        </div>
-        <div id="search">
-          <button
-            id="routeBtn"
-            onClick={() => {
-              router.push("/");
-            }}
-          >
-            <Image src={iconBack} alt="Icone botão voltar"></Image>
-          </button>
+	const router = useRouter();
+	return (
+		<main id="mainList">
+			<div id="leftDiv">
+				<Image src={logoUrna} alt="" id="logo"></Image>
+			</div>
+			<div id="rightDiv">
+				<div id="divTop">
+					<div id="left">
+						<h1>Listagem de Partidos</h1>
+					</div>
+					<div id="right">
+						<label htmlFor="">
+							<Image
+								src={iconUser}
+								alt="Ícone Usúario"
+								width={40}
+								height={40}
+							/>
+							<div>
+								<p>Júlio Cesar Alencar</p>
+								<a href="">
+									<span>Sair</span>
+								</a>
+							</div>
+						</label>
+					</div>
+				</div>
+				<div id="search">
+					<button
+						id="routeBtn"
+						onClick={() => {
+							router.push("/");
+						}}
+					>
+						<Image src={iconBack} alt="Icone botão voltar"></Image>
+					</button>
 
-          <label id="" htmlFor="">
-            <button type="button">
-              <Image src={iconFilter} alt="Filtro"></Image>
-            </button>
-            <input id="inputSearch" type="text" placeholder="Pesquisar" />
-          </label>
-        </div>
+					<label id="" htmlFor="">
+						<button type="button">
+							<Image src={iconFilter} alt="Filtro"></Image>
+						</button>
+						<input id="inputSearch" type="text" placeholder="Pesquisar" />
+					</label>
+				</div>
 
-        <div id="list">
-
-        </div>
-      </div>
-    </main>
-  );
+				<div id="list"></div>
+			</div>
+		</main>
+	);
 }
