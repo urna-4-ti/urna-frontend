4ff0027c30aaf71541ebfdf05539659165d8f180
FelipeTDR31 <felipetdrgames@gmail.com>
2024-04-03 21:55:49 -0300

    UD-8 [feat] add edit/delete government

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/display-name */
+import { InputHTMLAttributes, forwardRef, useId } from "react";
+
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+	label?: string;
+	helperText?: string;
+};
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+	(
+		{ type = "text", name = "", label = "", helperText = "", ...props },
+		ref,
+	) => {
+		const inputId = useId();
+		return (
+			<label htmlFor={inputId}>
+				<p>{label}</p>
+				<input type={type} name={name} ref={ref} {...props} />
+			</label>
+		);
+	},
+);
