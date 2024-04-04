bb94b244d55e19d2597ccecde10f3f8db01fcf97
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-04-04 01:30:05 -0300

    UD-2  [wip] listagem

--- src/app/admin/listarEleitor/page.tsx
+++ src/app/admin/listarEleitor/page.tsx
@@ -18,73 +18,73 @@ export default function Home() {
 				<Image id="urnaIf" src={ifLogo} alt="Urna"></Image>
 			</div>
 
-            <div className="right-panel">
-                <nav>
-                    <h1>Listagem de Eleitores</h1>
+			<div className="right-panel">
+				<nav>
+					<h1>Listagem de Eleitores</h1>
 
-                    <div className="info-admin">
-                        <div className="adm">
-                            <Image id="userIcon" src={userIcon} alt=""></Image>
-                            <span id="adminName">Administrador</span>
-                        </div>
-                        <button
-                            id="sair"
-                            onClick={() => {
-                                router.push("/");
-                            }}
-                        >
-                            Sair
-                        </button>
-                    </div>
-                </nav>
-                
-                <div className="filter-search">
-                    <button
-                        id="cancelButtonIcon"
-                        onClick={() => {
-                            router.back();
-                        }}
-                    >
-                        <Image src={iconBack} alt="Icone botão voltar"></Image>
-                    </button>
+					<div className="info-admin">
+						<div className="adm">
+							<Image id="userIcon" src={userIcon} alt=""></Image>
+							<span id="adminName">Administrador</span>
+						</div>
+						<button
+							id="sair"
+							onClick={() => {
+								router.push("/");
+							}}
+						>
+							Sair
+						</button>
+					</div>
+				</nav>
 
-                    <div className="act">
-                        <button id="filter">
-                            <Image src={iconFilter} alt="Icon filtragem"></Image>
-                        </button>
+				<div className="filter-search">
+					<button
+						id="cancelButtonIcon"
+						onClick={() => {
+							router.back();
+						}}
+					>
+						<Image src={iconBack} alt="Icone botão voltar"></Image>
+					</button>
 
-                        <div className="search">
-                            <Image src={iconSearch} alt="Icon busca"></Image>
-                            <input placeholder="Pesquisar..." type="text" id="busca" />
-                        </div>
-                    </div>
+					<div className="act">
+						<button id="filter">
+							<Image src={iconFilter} alt="Icon filtragem"></Image>
+						</button>
 
-                    <div className="pontomorto"></div>
-                </div>
+						<div className="search">
+							<Image src={iconSearch} alt="Icon busca"></Image>
+							<input placeholder="Pesquisar..." type="text" id="busca" />
+						</div>
+					</div>
 
-                <div className="table-contents">
-                    <div className="head">
-                        <p>Nome</p> <p>Matrícula</p> <p>Turma</p> <p>Email</p>
-                    </div>
-                    
-                    <div className="body-table">
-                        <div
-                            className="element"
-                            onClick={() => {
-                                router.push("/admin/editarEleitor");
-                            }}
-                        >
-                            <p>Luis Felipe Assmann</p>
-                            <p>2020304562</p>
-                            <p>4TI</p>
-                            <p>luis.assmannnnn@aluno.feliz.ifrs.edu.br</p>
-                            
-                            {/* Elemento armazenando a uuid do usuário para ancorar a tela de edição */}
-                            <span id="UUID" style={{ display: "none" }}></span>
-                        </div>   
-                    </div>
-                </div>
-            </div>
+					<div className="pontomorto"></div>
+				</div>
+
+				<div className="table-contents">
+					<div className="head">
+						<p>Nome</p> <p>Matrícula</p> <p>Turma</p> <p>Email</p>
+					</div>
+
+					<div className="body-table">
+						<div
+							className="element"
+							onClick={() => {
+								router.push("/admin/editarEleitor");
+							}}
+						>
+							<p>Luis Felipe Assmann</p>
+							<p>2020304562</p>
+							<p>4TI</p>
+							<p>luis.assmannnnn@aluno.feliz.ifrs.edu.br</p>
+
+							{/* Elemento armazenando a uuid do usuário para ancorar a tela de edição */}
+							<span id="UUID" style={{ display: "none" }}></span>
+						</div>
+					</div>
+				</div>
+			</div>
 		</main>
 	);
 }
