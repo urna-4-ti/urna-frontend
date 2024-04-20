b14c97e76072da332ce74d6e3073d7c71f4eb6c7
devvieira <kauavieira900@gmail.com>
2024-04-20 19:34:55 -0300

    UD-9  [WIP] teste

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,13 +4,12 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked async if using await inside
 export async function middleware(request: NextRequest) {
-		const queryParams = request.nextUrl.searchParams
-		request.cookies.set(
-			 "access_token",
-			 queryParams.get("access_token") as string
-		)
-		return NextResponse.next();
-	
+	const queryParams = request.nextUrl.searchParams;
+	request.cookies.set(
+		"access_token",
+		queryParams.get("access_token") as string,
+	);
+	return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
--- src/store/auth/index.ts
+++ src/store/auth/index.ts
@@ -1,78 +1,76 @@
-import { StoreProps } from "@/lib/types/store"
-import { userLogin } from "@/lib/types/user"
-import { api } from "@/requests/api"
-import { create } from "zustand"
-import { createJSONStorage, persist } from "zustand/middleware"
-import { jwtDecode } from "jwt-decode"
-
+import { StoreProps } from "@/lib/types/store";
+import { userLogin } from "@/lib/types/user";
+import { api } from "@/requests/api";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import { jwtDecode } from "jwt-decode";
 
 export type jwtType = {
- id:string,
- email:string,
- name:string
-}
+	id: string;
+	email: string;
+	name: string;
+};
 
 export const AuthStore = create<StoreProps>()(
-  persist(
-    (set) => ({
-      state: {
-        user: {
-          email: '',
-         name:'',
-         id:'',
-        },
-      },
-      actions: {
-        login: async (user): Promise<string> => {
-          let token = ''
-          const userData: userLogin = {
-            email: user.email,
-            password: user.password,
-          }
-          const formdata = new FormData()
-          formdata.append('email',userData.email)
-          formdata.append('password',userData.password)
+	persist(
+		(set) => ({
+			state: {
+				user: {
+					email: "",
+					name: "",
+					id: "",
+				},
+			},
+			actions: {
+				login: async (user): Promise<string> => {
+					let token = "";
+					const userData: userLogin = {
+						email: user.email,
+						password: user.password,
+					};
+					const formdata = new FormData();
+					formdata.append("email", userData.email);
+					formdata.append("password", userData.password);
 
-          const r = await api.post('auth/signIn',formdata,{
-            headers:{
-              "Content-Type":"multipart/form-data"
-            }
-          })
-          console.log(r.data);
+					const r = await api.post("auth/signIn", formdata, {
+						headers: {
+							"Content-Type": "multipart/form-data",
+						},
+					});
+					console.log(r.data);
 
-          const {accessToken} = r.data
+					const { accessToken } = r.data;
 
-      
-          
-          const {email, id, name}:jwtType = jwtDecode(accessToken)
+					const { email, id, name }: jwtType = jwtDecode(accessToken);
 
-          set({
-            state: {
-              user: {
-                id,
-                name,
-                email,
-              }
-            }
-          })
-          return accessToken
-        },
-        logout: () => {
-          set({
-            state: {
-              user: {
-                email: '',
-               id:'',name:''
-              },
-            },
-          })
-        },
-      },
-    }),
-    {
-      name: 'auth',
-      storage: createJSONStorage(() => localStorage),
-      partialize: ({ state }) => ({ state }),
-    },
-  ),
-)
+					set({
+						state: {
+							user: {
+								id,
+								name,
+								email,
+							},
+						},
+					});
+					return accessToken;
+				},
+				logout: () => {
+					set({
+						state: {
+							user: {
+								email: "",
+								id: "",
+								name: "",
+							},
+						},
+					});
+				},
+			},
+		}),
+		{
+			name: "auth",
+			storage: createJSONStorage(() => localStorage),
+			partialize: ({ state }) => ({ state }),
+		},
+	),
+);
