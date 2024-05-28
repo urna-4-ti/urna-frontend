85fa03364bb1e5c14e50248771338126a640743b
Rafael Mattos <rafael.vargas.emp@gmail.com>
2024-05-28 16:12:08 -0300

    ud-0 [feat] update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,29 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-import { cookies } from "next/headers";
-
-// This function can be marked async if using await inside
 export async function middleware(request: NextRequest) {
-	if (await request.cookies.get("access_token")) {
-		console.log("cookie exists");
+	const existingToken = await request.cookies.get("access_token");
 
+	if (existingToken) {
+		console.log("cookie exists");
 		return NextResponse.next();
 	}
-	if (request.nextUrl.searchParams.get("access_token")) {
+
+	const accessToken = request.nextUrl.searchParams.get("access_token");
+	if (accessToken) {
 		const queryParams = request.nextUrl.searchParams;
-		cookies().set({
+		const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // Set expiration in 7 days
+
+		return NextResponse.next().cookies.set({
 			name: "access_token",
-			value: queryParams.get("access_token") as string,
-			expires: 604800000, // 7 days
+			value: request.nextUrl.searchParams.get("access_token") as string,
+			expires,
 		});
-		request.nextUrl.searchParams.delete("access_token");
-		return NextResponse.next();
 	}
+
 	return NextResponse.redirect(new URL("/login", request.url));
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
 	matcher: "/admin/:path*",
 };
