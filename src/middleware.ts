a82d1c2fdbaf56c94a0f006b39374b488a7b7941
Rafael Mattos <rafael.vargas.emp@gmail.com>
2024-05-30 13:11:33 -0300

    middleware create a cookie now ( again )

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,23 @@ export async function middleware(request: NextRequest) {
 
 		return NextResponse.next();
 	}
+	const url = request.nextUrl;
+	const accessToken = url.searchParams.get("access_token");
 
+	if (accessToken) {
+		const response = NextResponse.next();
+		response.cookies.set("token", accessToken, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === "production",
+			sameSite: "strict",
+			maxAge: 60 * 60 * 24 * 7,
+		});
+
+		url.searchParams.delete("access_token");
+		const newUrl = url.toString();
+
+		return NextResponse.redirect(new URL(newUrl));
+	}
 	console.log("all failed, to login");
 
 	return NextResponse.redirect(new URL("/login", request.url));
