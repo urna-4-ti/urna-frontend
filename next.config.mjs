cc1d04f98b3decd201e9ca4ec3b5531457269701
Rafael Mattos <rafael.vargas.emp@gmail.com>
2024-06-07 16:58:24 -0300

    [fix] backend url

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
       },
       {
         protocol:'https',
-        hostname:"https://urna-apiv2-eelrne6u7a-rj.a.run.app",
+        hostname:"urna-apiv2-eelrne6u7a-rj.a.run.app",
         pathname: '/public/**',
 
       }
